17e25c80a7132d4676c4939d07cb87c74221aa65
kiranthilak <kiran.thilak@yahoo.com>
2022-09-05 18:29:09 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+function App() {
+  const [apiData, setApiData] = useState('')
+  useEffect( ()=> {
+    async function callApi(){
+    const apiresult = axios.get('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY')
+  console.log(apiresult) 
+  setApiData((await apiresult).data)
+  }
+  callApi();
+  //console.log(apiData);
+  }, [apiData])
+  return (
+    <div className='App'>
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>TITLE</th>
+        <th>BODY</th>
+
+
+      </tr>
+    </table>
+    
+      { apiData &&
+        (apiData).map((apiDetails, index)=>{
+          return(
+            <tr>
+            <td>{apiDetails.id}</td>
+            
+            </tr>
+          )
+        })
+      }
+      
+      
+    </div>
+  );
+}
+
+export default App;
